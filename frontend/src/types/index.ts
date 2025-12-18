7e7dc8ecc3d20c7ae995a90acddb23d3e462270a
Sahishnu <sahishnuk2@gmail.com>
2025-12-18 15:59:35 +0800

    Add the types of each component

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,32 @@
+// User
+export interface User {
+  id: number;
+  username: string;
+  password?: string;
+  created_at: string;
+}
+
+// Topic
+export interface Topic {
+  id: number;
+  title: string;
+  created_by: number;
+  created_at: string;
+}
+
+// Post
+export interface Post {
+  id: number;
+  title: string;
+  content: string;
+  created_by: number;
+  created_at: string;
+}
+
+// Comment
+export interface Comment {
+  id: number;
+  content: string;
+  created_by: number;
+  created_at: string;
+}
